28cd11eafc00bacafe168a2dd686266ba8896382
ArchKudo <vgd.vyas17@gmail.com>
2018-03-12 01:03:24 +0530

    Updated to have one more signal handler

--- hello_class.py
+++ hello_class.py
@@ -2,8 +2,8 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class HelloWindow(Gtk.Window):
 
+class HelloWindow(Gtk.Window):
     def __init__(self):
 
         # Call super init
@@ -11,15 +11,27 @@ class HelloWindow(Gtk.Window):
 
         # Create a button
         self.button = Gtk.Button(label='Click Me!')
-        # Add a handler for `clicked` event
-        self.button.connect('clicked', self.button_handler)
+
+        """
+        Add a handler for `clicked` event
+        General syntax:
+        widget.connect('event', callback, data)
+        """
+        self.button.connect('clicked', self.click_handler)
+
+        new_handler_id = self.button.connect('released', self.release_handler)
 
         # Add button to window as a child
         self.add(self.button)
 
     # Called when button is clicked
-    def button_handler(self, widget):
-        print('Hello, World!')
+    def click_handler(self, widget):
+        print('Button clicked!')
+
+    # Called when button is released
+    def release_handler(self, widget):
+        print('Button released!')
+
 
 win = HelloWindow()
 win.connect('delete-event', Gtk.main_quit)
