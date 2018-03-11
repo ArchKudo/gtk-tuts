4dea0141e85780ae04d5c5de97b98bea7cfe45fa
ArchKudo <vgd.vyas17@gmail.com>
2018-03-12 01:30:41 +0530

    Added getting and setting properties

--- hello_class.py
+++ hello_class.py
@@ -2,8 +2,8 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class HelloWindow(Gtk.Window):
 
+class HelloWindow(Gtk.Window):
     def __init__(self):
 
         # Call super init
@@ -14,12 +14,35 @@ class HelloWindow(Gtk.Window):
         # Add a handler for `clicked` event
         self.button.connect('clicked', self.button_handler)
 
+        # Get all properties
+        print(dir(self.button.props))
+
+        # Get specific property
+        print(self.button.get_property('label'))
+
+        # Change some arbitary properties
+        self.button.set_property('label', 'Mellow, World!!')
+
+        # Change alignment
+        print('({},{})'.format(
+            self.button.get_property('xalign'),
+            self.button.get_property('yalign')))
+        
+        self.button.set_property('xalign', 0.7)
+        self.button.set_property('yalign', 0.9)
+
+        # Change margin
+        print(self.button.get_property('margin'))
+
+        self.button.set_property('margin', 10)
+
         # Add button to window as a child
         self.add(self.button)
 
     # Called when button is clicked
     def button_handler(self, widget):
         print('Hello, World!')
+
 
 win = HelloWindow()
 win.connect('delete-event', Gtk.main_quit)
